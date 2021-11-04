b2dd6f336f1ba40e43fd0fa2ee60a8e956349da0
kmasiak <38893432+kmasiak@users.noreply.github.com>
2021-11-03 22:58:34 -0400

    intial project setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+class HelloMessage extends React.Component {
+  render() {
+    return React.createElement("div", null, "Hello ", this.props.name);
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  React.createElement(HelloMessage, { name: "Jakob" }),
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
